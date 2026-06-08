bf302b00cb14cec39b7a4b676d8d2b5f0b674c63
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-08 09:27:46 +0400

    Task 38: Understanding Java POST Operations – Add a New Employee Record

---
+++ src/main/java/com/example/Task8Demo/Employee.java
@@ -0,0 +1,18 @@
+package com.example.Task8Demo;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+public class Employee {
+    private String employeeId;
+    private String employeeName;
+    private String dDepartment;
+
+}
---
+++ src/main/java/com/example/Task8Demo/EmployeeService.java
@@ -0,0 +1,51 @@
+package com.example.Task8Demo;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class EmployeeService {
+
+    static List<Employee> employeeList = new ArrayList<>();
+
+    static public void load(){
+        employeeList.add(new Employee("E101","Ahmed","IT"));
+        employeeList.add(new Employee("E102","Fatma","HR"));
+        employeeList.add(new Employee("E103","Khawla","Operation"));
+    }
+
+    public static void displayEmployee(){
+        for(Employee emp : employeeList){
+            System.out.println(emp);
+        }
+    }
+    public static Boolean validateData(String id){
+        for(Employee emp : employeeList){
+            if(emp.getEmployeeId().equalsIgnoreCase(id)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void addEmployee(String name , String id ,String department){
+        if(validateData(id)){
+            employeeList.add(new Employee(id,name,department));
+            System.out.println("Employee added successfully");
+        }else{
+            System.out.println("Employee with this Id already exist");
+        }
+
+    }
+    public static void main(String[] args) {
+        load();
+        displayEmployee();
+        addEmployee("reem", "E1066" , "IT");
+        displayEmployee();
+
+    }
+
+
+}
