5f80b24d525ff027cdd1891fe1fe33f2091e4c4d
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-08 09:03:13 +0400

    first commit

---
+++ src/main/java/com/example/Task8Demo/Task8DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Task8Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task8DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Task8DemoApplication.class, args);
+	}
+
+}
