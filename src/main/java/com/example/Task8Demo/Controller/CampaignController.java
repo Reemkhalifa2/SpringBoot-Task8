d3c6f70e195903409b235c208c1a736c95a50bae
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-09 11:53:56 +0400

    update Campaign Controller

--- src/main/java/com/example/Task8Demo/Controller/CampaignController.java
+++ src/main/java/com/example/Task8Demo/Controller/CampaignController.java
@@ -1,29 +1,43 @@
 package com.example.Task8Demo.Controller;
 
 import com.example.Task8Demo.Entity.Campaign;
-import com.example.Task8Demo.Service.CampaignManager;
+import com.example.Task8Demo.Service.CampaignService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("campaign")
 public class CampaignController {
 
     @Autowired
-    CampaignManager campaignManager;
+    CampaignService campaignService;
 
-    @PostMapping("addCampaign")
-    public String addCampaign(@RequestBody Campaign campaign){
-        return campaignManager.addCampaign(campaign);
+    @PostMapping("add")
+    public Campaign save(Campaign campaign){
+        return campaignService.save(campaign);
     }
 
-    @GetMapping("display")
-    public List<Campaign> displayCampaigns(){
-        return campaignManager.displayCampaign();
+    @GetMapping("getAll")
+    public List<Campaign> getAll(){
+        return campaignService.getAll();
+    }
+
+    @GetMapping("GetById")
+    public Campaign getById(Integer id){
+        return campaignService.getById(id);
+    }
+
+    @PutMapping("update")
+    public Campaign update(Integer id , Campaign campaign){
+        return campaignService.update(id,campaign);
+    }
+
+    @DeleteMapping("delete")
+    public String delete(Integer id){
+        campaignService.delete(id);
+        return "Deleted!";
     }
 
 }
