afe509d4b10a640f3f5fdd067daf249dd8878c94
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-10 14:26:56 +0400

    Refactor Campaign class to extend ParentEntity and remove unused imports

--- src/main/java/com/example/Task8Demo/Entity/Campaign.java
+++ src/main/java/com/example/Task8Demo/Entity/Campaign.java
@@ -1,7 +1,6 @@
 package com.example.Task8Demo.Entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,14 +8,10 @@ import lombok.Setter;
 @Setter
 
 @Entity
-public class Campaign {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer campaignId;
+public class Campaign extends ParentEntity {
     private String campaignName;
     private String platform;
     private Double budget;
-
     @OneToOne
     Employee employee;
 }
