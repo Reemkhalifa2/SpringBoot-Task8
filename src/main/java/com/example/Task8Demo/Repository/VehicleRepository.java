5e7d0122b2cbf650fa9bad691e722a3c51da9944
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-10 15:00:51 +0400

    Add queries to VehicleRepository for active vehicles

--- src/main/java/com/example/Task8Demo/Repository/VehicleRepository.java
+++ src/main/java/com/example/Task8Demo/Repository/VehicleRepository.java
@@ -2,8 +2,24 @@ package com.example.Task8Demo.Repository;
 
 import com.example.Task8Demo.Entity.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @Repository
 public interface VehicleRepository extends JpaRepository<Vehicle,Integer> {
+    @Query("SELECT v FROM Vehicle WHERE v.isActive=true ")
+    List<Vehicle> getAll();
+
+    @Query("SELECT v FROM Vehicle WHERE v.isActive=true and v.id=:id")
+    Vehicle getById(@Param("id") Integer id);
+
+    @Query("SELECT v FROM Vehicle WHERE v.isActive=true and v.vehicleModel=:vehicleModel")
+    List<Vehicle> getByModel(@Param("vehicleModel") String vehicleModel);
+
+
+
 }
