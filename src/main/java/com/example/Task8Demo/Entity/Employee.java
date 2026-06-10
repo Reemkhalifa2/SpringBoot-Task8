8e52abfd5cbae39b88a349a7723d1dd7818ce078
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-10 16:30:24 +0400

    Refactor Employee class: replace Lombok annotations with @Data and @NoArgsConstructor

--- src/main/java/com/example/Task8Demo/Entity/Employee.java
+++ src/main/java/com/example/Task8Demo/Entity/Employee.java
@@ -4,18 +4,18 @@ package com.example.Task8Demo.Entity;
 import com.example.Task8Demo.Repository.EmployeeRepository;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.Data;
+
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.ValueGenerationType;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
-@Getter
-@Setter
-@ToString
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
 @Entity
 public class Employee extends ParentEntity{
     private String employeeName;
