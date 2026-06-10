dab05bd73030cbb1c0772a2692fed0bbf0f22e60
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-10 14:27:29 +0400

    Add methods to retrieve active employees in EmployeeRepository

--- src/main/java/com/example/Task8Demo/Repository/EmployeeRepository.java
+++ src/main/java/com/example/Task8Demo/Repository/EmployeeRepository.java
@@ -2,8 +2,18 @@ package com.example.Task8Demo.Repository;
 
 import com.example.Task8Demo.Entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
+
+    @Query("SELECT e FROM Employee e WHERE e.isActive = true")
+    List<Employee> getAll();
+
+    @Query("SELECT e FROM Employee e WHERE e.id=:id AND e.isActive = true")
+    Employee getById(@Param(value = "id") Integer id);
 }
