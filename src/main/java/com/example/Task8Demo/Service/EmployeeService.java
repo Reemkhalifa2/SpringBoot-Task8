ae12716bccfb45378675a7b1dd7c1b832a1ed0f2
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-10 14:27:38 +0400

    Enhance EmployeeService: update and delete methods with null checks and update date

--- src/main/java/com/example/Task8Demo/Service/EmployeeService.java
+++ src/main/java/com/example/Task8Demo/Service/EmployeeService.java
@@ -6,11 +6,14 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.xml.crypto.Data;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Service
 public class EmployeeService {
+    List<Employee> deletedEmployee = new ArrayList<>();
 
     @Autowired
     EmployeeRepository employeeRepository;
@@ -21,22 +24,35 @@ public class EmployeeService {
     }
 
     public Employee save(Employee employee){
+
         return employeeRepository.save(employee);
     }
 
     public List<Employee> getAll(){
-        return employeeRepository.findAll();
+        return employeeRepository.getAll();
     }
 
-    public Employee update(Integer id, Employee newEmployee) {
-        Employee emp = getById(id);
-        emp.setEmployeeName(newEmployee.getEmployeeName());
-        emp.setDepartment(newEmployee.getDepartment());
-        return employeeRepository.save(emp);
+    public Employee update(Integer id, Employee newEmployee) throws Exception {
+        Employee emp = employeeRepository.getById(id);
+        if(emp != null){
+            emp.setEmployeeName(newEmployee.getEmployeeName());
+            emp.setDepartment(newEmployee.getDepartment());
+            emp.setUpdateDate(new Date());
+            return employeeRepository.save(emp);
+        }
+        throw new Exception("Employee Cannot be Null!");
+
     }
 
-    public void delete(Integer id) {
-        employeeRepository.delete(getById(id));
+    public Boolean delete(Integer id) {
+        Employee employee = employeeRepository.getById(id);
+        if(employee != null){
+            employee.setIsActive(false);
+            employee.setUpdateDate(new Date());
+            employeeRepository.save(employee);
+            return true;
+        }
+        return false;
     }
 
 
